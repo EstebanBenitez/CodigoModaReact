fc2ed92c644550fcc182bd1fef1cff0841118740
EstebanHernandez <juanestebanhernandesbvenitez@gmail.com>
2022-12-20 13:34:28 -0500

    se sube registro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{Fragment, useState} from 'react';
 
-function App() {
+
+
+function Formulario() {
+  
+  const [Relaciones,setDatos] =useState({
+    nombre: ``,
+    apellido: ``,
+    gustos:``,
+    clave:``,
+    correo:``
+  })
+
+  const registros =(event)=>{
+    console.log (event.target.value )
+    setDatos({
+      ...Relaciones,
+
+      [event.target.value ]:event.target.value
+    })
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+< div className = "container">
+   <Fragment>
+       <h1>Registro</h1>
+       <form className='row'>
+
+         <div className="">
+          <input placeholder='Ingrese Nombre'
+          className='from-control'
+          type= "text"  
+           name="nombre" 
+           onChange={registros}>
+          </input>
+          </div>
+        
+          <div className="">
+          <input
+           placeholder='Ingrese Apellido '
+           className='from-control'
+           type="text"
+           name="apellido"
+           onChange={registros}>
+           </input>
+          </div>
+
+
+          <div className="">
+          <input
+           placeholder='Ingrese Tus Gustos '
+           className='from-control'
+           type="text"
+           name="gustos"
+           onChange={registros}>
+           </input>
+          </div>
+
+          <div className="">
+          <input
+           placeholder='clave '
+           className='from-control'
+           type="password"
+           name="calve"
+           onChange={registros} >
+           </input>
+          </div> 
+
+
+          
+          <div className="">
+          <input
+           placeholder='Ingrese correo '
+           className='from-control'
+           type="email"
+           name="email"
+           onChange={registros}>
+           </input>
+          </div> 
+
+          <div className="">
+          <button className='btn btn-primary' type='submit'>Registrame</button>
+          </div>
+
+
+       </form>
+       
+    
+    
+    </Fragment>
+   </div>
   );
 }
 
-export default App;
+export default Formulario;
